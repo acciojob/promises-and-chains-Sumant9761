04c73e4ba067e54f64d243edd0843a722d66c02c
Sumant9761 <142734516+Sumant9761@users.noreply.github.com>
2026-06-10 22:17:54 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,28 @@
 //your JS code here. If required.
+document.getElementById("btn").addEventListener("click", function () {
+    const age = document.getElementById("age").value;
+    const name = document.getElementById("name").value;
+
+    if (age === "" || name === "") {
+        alert("Please enter valid details.");
+        return;
+    }
+
+    const promise = new Promise((resolve, reject) => {
+        setTimeout(() => {
+            if (Number(age) > 18) {
+                resolve(`Welcome, ${name}. You can vote.`);
+            } else {
+                reject(`Oh sorry ${name}. You aren't old enough.`);
+            }
+        }, 4000);
+    });
+
+    promise
+        .then((message) => {
+            alert(message);
+        })
+        .catch((message) => {
+            alert(message);
+        });
+});
